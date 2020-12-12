237d8f84bd6bc2e80abfd904c11c86bad6fa0bff
PeterCartine <Peter.Cartine@gmail.com>
2020-12-12 15:36:13 -0500

    Fixed the value_betting_calculator in pokeroddscalculator.py and tested some ideas for the preflop starting hands in messaround.py

--- RunGame.py
+++ RunGame.py
@@ -5,9 +5,9 @@ from PeterPokerBot3 import *
 if __name__ == '__main__':
     hand_numbers = []
     for x in range(0, 1000):
-        player1 = CPUPlayer2(1000, "Player 1")
+        player1 = CPUPlayer(1000, "Player 1")
         from PeterPokerBot2 import CPUPlayer2
-        player2 = CPUPlayer3(1000, "Player 2")
+        player2 = CPUPlayer2(1000, "Player 2")
         print()
         print()
         hand_numbers.append(play_holdem(player1, player2, 5, 10))
